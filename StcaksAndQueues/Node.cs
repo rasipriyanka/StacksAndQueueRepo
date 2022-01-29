2c684820c548c13727125b7557fe9bf3a0cce605
priyanka <priyankapendela111@gmail.com>
2022-01-29 21:59:38 +0530

    [Rasi]Added the implementation For Stacks

---
+++ StcaksAndQueues/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StcaksAndQueues
+{
+    internal class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int d) // creating Constructer
+        {
+            data = d;
+            next = null;
+        }
+    }
+}
+
--- StcaksAndQueues/Program.cs
+++ StcaksAndQueues/Program.cs
@@ -6,7 +6,11 @@ namespace StcaksAndQueues
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stacks linkedListStack = new Stacks();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
         }
     }
 }
