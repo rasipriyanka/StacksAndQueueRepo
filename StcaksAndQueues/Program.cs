167ab1dc137abe6278f704c1d1e930f51fc5da24
priyanka <priyankapendela111@gmail.com>
2022-01-29 22:04:06 +0530

    [Rasi]Added the implementation

--- StcaksAndQueues/Program.cs
+++ StcaksAndQueues/Program.cs
@@ -6,11 +6,13 @@ namespace StcaksAndQueues
     {
         static void Main(string[] args)
         {
-            Stacks linkedListStack = new Stacks();
-            linkedListStack.Push(70);
+            Stacks linkedListStack = new Stacks(); // creating object for a class
+            linkedListStack.Push(70);    // adding values to the stack
             linkedListStack.Push(30);
             linkedListStack.Push(56);
             linkedListStack.Display();
+            linkedListStack.Peek();
+            linkedListStack.Pop();
         }
     }
 }
